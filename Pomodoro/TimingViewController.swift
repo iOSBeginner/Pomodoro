1e23d8eda55468d1298b787c47cfaace5b181927
蘇健豪 <jenhausu@icloud.com>
2017-01-04 22:59:33 +0800

    開始暫停功能

--- Pomodoro/TimingViewController.swift
+++ Pomodoro/TimingViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class TimingViewController: UIViewController {
-
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    var status : String? //working, rest, nothing
+    var timer : Timer?
+    var remainTime : Int! = 300
+    var oneTimeUnit : Int! = 300
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        oneTimeUnit = 300
+        remainTime = oneTimeUnit
+        
+        status = "nothing"
+        
+        minuteLabel.text = String(remainTime! / 60)
+        secondLabel.text = String(remainTime! % 60)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        if status == "nothing" || status == "rest" {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+            sender.setTitle("暫停", for: .normal)
+            status = "working"
+        }
+        else if status == "working" {
+            timer?.invalidate()
+            sender.setTitle("開始", for: .normal)
+            status = "rest"
+        }
+    }
     }
     
 
@@ -29,7 +52,16 @@ class TimingViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    // MARK: - Timer
+    
+    @objc fileprivate func countDown() {
+        remainTime = remainTime! - 1
+        
+        minuteLabel.text = String(remainTime! / 60)
+        secondLabel.text = String(remainTime! % 60)
+        
+        if remainTime == 0 {
+            timer?.invalidate()
+        }
     }
-    */
-
 }
